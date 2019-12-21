5d622e793f35902d668d0c6493cbafef0b83ac64
Srini <srinigvasan@gmail.com>
2019-12-21 15:42:06 +0800

    Mvn test update

--- src/test/java/com/example/demo/SareetaApplicationTests.java
+++ src/test/java/com/example/demo/SareetaApplicationTests.java
@@ -11,7 +11,7 @@ import org.junit.BeforeClass;
 import org.hamcrest.number.IsNaN;
 
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeAll;
+
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
